4bf6401f89b9426308fbdebb38a926ed2a1336da
y-miyane <y.miyane926@gmail.com>
2025-06-27 18:13:02 +0900

    6/27

---
+++ src/food-app.ts
@@ -0,0 +1,15 @@
+class Score { }
+class Food {
+    constructor(public element: HTMLDivElement){
+        element.addEventListener('click');
+     }
+ }
+class Foods {
+    elements = document.querySelectorAll<HTMLDivElement>('.food');
+    constructor() {
+        this.elements.forEach(element =>{
+            new Food(element);
+        })
+    }
+ }
+const foods = new Foods();
