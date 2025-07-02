88c0072296aa6edbacd0a1f4f38d7073eda38c4e
宮根優美子 <y-miyane088@jutjoy.com>
2025-07-02 14:29:19 +0900

    7/2

--- dist/food-app.js
+++ dist/food-app.js
@@ -20,9 +20,20 @@ class Foods {
         });
         return this._activeElements;
     }
+    get activeElementsScore() {
+        this._activeElementsScore = [];
+        this.activeElements.forEach(element => {
+            const foodScore = element.querySelector('.food__score');
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        });
+        return this._activeElementsScore;
+    }
     constructor() {
         this.elements = document.querySelectorAll('.food');
         this._activeElements = [];
+        this._activeElementsScore = [];
         this.elements.forEach(element => {
             new Food(element);
         });
--- src/food-app.ts
+++ src/food-app.ts
@@ -10,6 +10,7 @@ class Food {
 class Foods {
     elements = document.querySelectorAll<HTMLDivElement>('.food');
     private _activeElements: HTMLDivElement[] = [];
+    private _activeElementsScore: number[] = [];
     get activeElements() {
         this._activeElements = [];
         this. elements.forEach(element => {
@@ -18,6 +19,16 @@ class Foods {
             }
         })
         return this._activeElements;
+    }
+    get activeElementsScore(){
+        this._activeElementsScore = [];
+        this.activeElements.forEach(element =>{
+            const foodScore = element.querySelector('.food__score');
+            if (foodScore){
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        })
+        return this._activeElementsScore;
     }
     constructor(){
         this.elements.forEach(element => {
